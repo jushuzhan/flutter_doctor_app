49874a5bbd0042f3a65c0cb39361443e0d1d4235
巨淑展 <jushuzhan@laiba.com>
2023-02-16 09:27:03 +0800

    修改底部退出按钮样式。

--- lib/common/view/ExitBottomDialog.dart
+++ lib/common/view/ExitBottomDialog.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
+
 typedef _CallBack = void Function();
+
 class ExitBottomDialog extends StatefulWidget {
   final _CallBack callBack;
+
   ExitBottomDialog({
-  super.key,
-  required this.callBack,
+    super.key,
+    required this.callBack,
   });
-
-
 
   @override
   _ExitBottomDialogState createState() {
-   return _ExitBottomDialogState();
+    return _ExitBottomDialogState();
   }
-
 }
 
 class _ExitBottomDialogState extends State<ExitBottomDialog> {
   _dismissDialog() {
     Navigator.of(context).pop();
   }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -33,53 +34,80 @@ class _ExitBottomDialogState extends State<ExitBottomDialog> {
               child: Container(),
             ),
           ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(16.0),
-                  topRight: Radius.circular(16.0)),
-            ),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 14),
-                    child: GestureDetector(
-                      child: Text(
-                        '立即退出',
-                        style:
-                            TextStyle(fontSize: 16, color: Color(0xFFE65D4E)),
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _dismissDialog();
+                          if (widget.callBack != null) {
+                            widget.callBack();
+                          }
+                        },
+                        child: Text(
+                          '立即退出',
+                          style:
+                              TextStyle(fontSize: 16, color: Color(0xFFE65D4E)),
+                        ),
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(16.0),
+                                  topRight: Radius.circular(16.0)),
+                            ),
+                          ),
+                          elevation: MaterialStateProperty.all(0),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                        ),
                       ),
-                      onTap: (){
-                        _dismissDialog();
-                        if(widget.callBack!=null){
-                          widget.callBack();
-                        }
-
-                      },
-                    )),
-                Divider(
-                  color: Color(0xFFEEEEEE),
-                  height: 1,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 14),
-                  child: GestureDetector(
-                    child: Text(
-                      '取消',
-                      style: TextStyle(fontSize: 16, color: Color(0xFF009999)),
+                      height: 50,
                     ),
-                    onTap: () {
-                      _dismissDialog();
-                    },
-                  ),
-                ),
-              ],
-            ),
+                    flex: 1,
+                  )
+                ],
+              ),
+              Divider(
+                color: Color(0xFFEEEEEE),
+                height: 0.5,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      child: ElevatedButton(
+                        onPressed: () => _dismissDialog(),
+                        child: Text(
+                          '取消',
+                          style:
+                              TextStyle(fontSize: 16, color: Color(0xFF009999)),
+                        ),
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.zero),
+                            ),
+                          ),
+                          elevation: MaterialStateProperty.all(0),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                        ),
+                      ),
+                      height: 50,
+                    ),
+                    flex: 1,
+                  )
+                ],
+              ),
+            ],
           ),
         ],
       ),
     );
   }
 }
-
